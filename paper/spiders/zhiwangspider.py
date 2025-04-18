0cc62f77b33b178d2a6f3bd965dce5ca823dc8d6
iam51XX <1370280216@qq.com>
2025-04-18 16:15:42 +0800

    changes

--- paper/spiders/zhiwangspider.py
+++ paper/spiders/zhiwangspider.py
@@ -14,70 +14,47 @@ root_path = os.getcwd()
 class ZhiwangspiderSpDDider(scrapy.Spider):
     name = "zhiwangspider"
     allowed_domains = ["kns.cnki.net"]          #限定爬虫的网站
-    start_urls = ["https://kns.cnki.net/kns8s/"]
-    # todo
-    # cookies = {
-    #     'show_vpn': '1',
-    #     'heartbeat': '1',
-    #     'show_faq': '0',
-    #     'Hm_lvt_2e552fbaf5e9c8ff5d407c2048cc91bb': '1744723856,1744867867,1744942935,1744944787',
-    #     'Hm_lpvt_2e552fbaf5e9c8ff5d407c2048cc91bb': '1744944787',
-    #     'HMACCOUNT': 'FC9D3909A3EBF658',
-    #     'wengine_vpn_ticketwebvpn_shmtu_edu_cn': 'b7b55427970f14ab',
-    #     'refresh': '0'
-    # }
-    cookie_str = """
-    Ecp_notFirstLogin=aRoA2H; Ecp_ClientId=1241030224001677371; Hm_lvt_dcec09ba2227fd02c55623c1bb82776a=1730299402; cnkiUserKey=dc14bdd7-1581-8535-94d4-3532eda3f653; Ecp_IpLoginFail=250418124.76.103.42; SID_kns_new=kns2618132; LID=WEEvREcwSlJHSldTTEYyUFcra3BFQkV4cnpGQkIzOWNUbnlGYU9pMTd2VT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!; Ecp_loginuserjf=18964800158; Ecp_LoginStuts={"IsAutoLogin":false,"UserName":"18964800158","ShowName":"18964800158","UserType":"jf","BUserName":"","BShowName":"","BUserType":"","r":"aRoA2H","Members":[]}; Ecp_session=1; updatetime-advInput=2025-04-18+09%3A44%3A47; knsadv-searchtype=%7B%22BLZOG7CK%22%3A%22gradeSearch%2CmajorSearch%22%2C%22MPMFIG1A%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22T2VC03OH%22%3A%22gradeSearch%2CmajorSearch%22%2C%22JQIRZIYA%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22S81HNSV3%22%3A%22gradeSearch%22%2C%22YSTT4HG0%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22ML4DRIDX%22%3A%22gradeSearch%2CmajorSearch%22%2C%22WQ0UVIAA%22%3A%22gradeSearch%2CmajorSearch%22%2C%22VUDIXAIY%22%3A%22gradeSearch%2CmajorSearch%22%2C%22NN3FJMUV%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22LSTPFY1C%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22HHCPM1F8%22%3A%22gradeSearch%2CmajorSearch%22%2C%22OORPU5FE%22%3A%22gradeSearch%2CmajorSearch%22%2C%22WD0FTY92%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22BPBAFJ5S%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22EMRPGLPA%22%3A%22gradeSearch%2CmajorSearch%22%2C%22PWFIRAGL%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22U8J8LYLV%22%3A%22gradeSearch%2CmajorSearch%22%2C%22R79MZMCB%22%3A%22gradeSearch%22%2C%22J708GVCE%22%3A%22gradeSearch%2CmajorSearch%22%2C%22HR1YT1Z9%22%3A%22gradeSearch%2CmajorSearch%22%2C%22JUP3MUPD%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22NLBO1Z6R%22%3A%22gradeSearch%2CmajorSearch%22%2C%22RMJLXHZ3%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%221UR4K4HZ%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22NB3BWEHK%22%3A%22gradeSearch%2CmajorSearch%22%2C%22XVLO76FD%22%3A%22gradeSearch%2CmajorSearch%22%7D; SID_restapi=018107; personsets=%E7%94%A8%E6%88%B7%E5%81%8F%E5%A5%BD%E8%AE%BE%E7%BD%AE%E4%B8%BA%E7%A9%BA%EF%BC%81; createtime-advInput=2025-04-18%2011%3A56%3A42; dblang=both; c_m_LinID=LinID=WEEvREcwSlJHSldTTEYyUFcra3BFQkV4cnpGQkIzOWNUbnlGYU9pMTd2VT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&ot=05%2F18%2F2025%2011%3A56%3A06; c_m_expire=2025-05-18%2011%3A56%3A06; tfstk=goboGw1tTg-7BZBtet87jNTkijZvPbTBV93ppepU0KJXyHpRTpjhUTxev79dnJXcOvuJzwjj-11Cy7TLNU1WReyTBPphN_TIYmJbr-C4TBTHaq4A2aCWReyAroSeM_6W-nMV8ePDuCd6Te-e8qPDsQYEzHur0xJXTe8ea3Rq0Qdia2JFLsPDOKJe8ekKV7JRFOQ4hipjEXJpeZAkqd50f2uh9VKkQ_Jm8R7DZJ9NaK0E8dArbiCcw801MU1VICB8u4XG1Z6DgOz3ne6fY9Rh3PMpPGB55nfbbq5vrKtw_ZPE7pQlD1JDSXzPiUvkbaKr92BNi6S9mZFa5UYyUML59f2RiaXRNZfKTm8kyixGrFzsUpIAswAhRJacI1_fnIWa-8SyFmoZajuBgWQqADtyGIv9OTdgilIe91P0ic-B4IO7oSVmADtyGIvTiSmwA3RXNrf..
-    """
-    # 转换为字典
-    cookies = dict(item.split("=", 1) for item in cookie_str.strip().split("; "))
 
-    # todo
-    """
-    这里修改了文件导入结构，
-    从绝对路径改为相对路径，
-    这样在所有人的电脑上都能运行
-    """
+    cookies = {
+        "Ecp_ClientId": "h250415025900118829",
+        "Ecp_IpLoginFail": "250418139.227.167.167",
+        "SID_kns_new": "kns2618105",
+        "updatetime-advInput": "2025-04-18 09:44:47",
+        "knsadv-searchtype": '{"BLZOG7CK":"gradeSearch,majorSearch", "MPMFIG1A":"gradeSearch,majorSearch,sentenceSearch", "T2VC03OH":"gradeSearch,majorSearch", "JQIRZIYA":"gradeSearch,majorSearch,sentenceSearch", "S81HNSV3":"gradeSearch", "YSTT4HG0":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "ML4DRIDX":"gradeSearch,majorSearch", "WQ0UVIAA":"gradeSearch,majorSearch", "VUDIXAIY":"gradeSearch,majorSearch", "NN3FJMUV":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "LSTPFY1C":"gradeSearch,majorSearch,sentenceSearch", "HHCPM1F8":"gradeSearch,majorSearch", "OORPU5FE":"gradeSearch,majorSearch", "WD0FTY92":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "BPBAFJ5S":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "EMRPGLPA":"gradeSearch,majorSearch", "PWFIRAGL":"gradeSearch,majorSearch,sentenceSearch", "U8J8LYLV":"gradeSearch,majorSearch", "R79MZMCB":"gradeSearch", "J708GVCE":"gradeSearch,majorSearch", "HR1YT1Z9":"gradeSearch,majorSearch", "JUP3MUPD":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "NLBO1Z6R":"gradeSearch,majorSearch", "RMJLXHZ3":"gradeSearch,majorSearch,sentenceSearch", "1UR4K4HZ":"gradeSearch,majorSearch,authorSearch,sentenceSearch", "NB3BWEHK":"gradeSearch,majorSearch", "XVLO76FD":"gradeSearch,majorSearch"}',
+        "createtime-advInput": "2025-04-18 13:54:49",
+        "knsadvisearchtype": "gDoCkPaXdjDCcKq91XRchA==",
+        "SID_sug": "018104",
+        "dblang": "both",
+        "tfstk": "gAnmKT0zXqzbwyGdMbrX-DMLxvL8GtZ_TfIT6lFwz7P7DPFxQ50igj4aWKNtZCcoC1SY0EyG7VlHhRdjXtgS9vSxDEZOjoZ_bBdpvHexhlZw7siwysazIJKOb5PacukKaQTHvHHj387GAPtKXr_fSSVabrya4TP7dsyZ_G-yERyOgNSq_YJuN-5a35Sw4uy_plPZ_lJlzRPz3xqV4SruY0RSygBFfYnjq-40TYYCjGR7e6FI3eIGb00lbWDV5GSZq-4m4xjFi_zx7vaIDmKGVoHnzuyKhnjuYxynM7heYgqreYlTR4O1vzGojyi4uORUTvDq8uo9Q9aoaR4UbzXNbYqbaXm4YH5uF4kx7mGcIGyjDDUgOzvNfPErvyuEiO9IUoyZ1yi9OiNqQviIJkxRukko7X0G4yQP8PLCf8Jt4N__u8wu9RGYKLmEafkkELb3Ary7ns9kEN__u8wu9Lvl-bU4F-ff."
+
+    }
 
     school = pandas.read_csv(os.path.join(root_path,"../school.csv"))
     beida = pandas.read_excel(os.path.join(root_path,"../北核期刊.xlsx"))
+    nanda = pandas.read_excel(os.path.join(root_path,"../南核期刊.xlsx"))
 
-    # todo
-    # headers = {
-    #
-    #     'Accept': '*/*',
-    #     'Accept-Encoding': 'gzip, deflate, br, zstd',
-    #     'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    #     'Connection': 'keep-alive',
-    #     'Content-Length': '1868',  # 可以删掉这个，Scrapy 会自动处理
-    #     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    #     'Origin': 'https://webvpn.shmtu.edu.cn',
-    #     'Referer': 'https://webvpn.shmtu.edu.cn/https/77726476706e69737468656265737421fbf952d2243e635930068cb8/kns8s/AdvSearch?crossids=YSTT4HG0%2CLSTPFY1C%2CJUP3MUPD%2CMPMFIG1A%2CWQ0UVIAA%2CBLZOG7CK%2CPWFIRAGL%2CEMRPGLPA%2CNLBO1Z6R%2CNN3FJMUV',
-    #     'Sec-Ch-Ua': '"Microsoft Edge";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
-    #     'Sec-Ch-Ua-Mobile': '?0',
-    #     'Sec-Ch-Ua-Platform': '"Windows"',
-    #     'Sec-Fetch-Dest': 'empty',
-    #     'Sec-Fetch-Mode': 'cors',
-    #     'Sec-Fetch-Site': 'same-origin',
-    #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0',
-    #     'X-Requested-With': 'XMLHttpRequest'
-    # }
     headers = {
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "Accept-Encoding": "gzip, deflate, br, zstd",
-        "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
-        "Cache-Control": "max-age=0",
-        "Connection": "keep-alive",
-        "Cookie": "Ecp_notFirstLogin=aRoA2H; Ecp_ClientId=1241030224001677371; Hm_lvt_dcec09ba2227fd02c55623c1bb82776a=1730299402; cnkiUserKey=dc14bdd7-1581-8535-94d4-3532eda3f653; Ecp_IpLoginFail=250418124.76.103.42; SID_kns_new=kns2618132; LID=WEEvREcwSlJHSldTTEYyUFcra3BFQkV4cnpGQkIzOWNUbnlGYU9pMTd2VT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!; Ecp_loginuserjf=18964800158; Ecp_LoginStuts={\"IsAutoLogin\":false,\"UserName\":\"18964800158\",\"ShowName\":\"18964800158\",\"UserType\":\"jf\",\"BUserName\":\"\",\"BShowName\":\"\",\"BUserType\":\"\",\"r\":\"aRoA2H\",\"Members\":[]}; Ecp_session=1; updatetime-advInput=2025-04-18+09%3A44%3A47; knsadv-searchtype=%7B%22BLZOG7CK%22%3A%22gradeSearch%2CmajorSearch%22%2C%22MPMFIG1A%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22T2VC03OH%22%3A%22gradeSearch%2CmajorSearch%22%2C%22JQIRZIYA%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22S81HNSV3%22%3A%22gradeSearch%22%2C%22YSTT4HG0%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22ML4DRIDX%22%3A%22gradeSearch%2CmajorSearch%22%2C%22WQ0UVIAA%22%3A%22gradeSearch%2CmajorSearch%22%2C%22VUDIXAIY%22%3A%22gradeSearch%2CmajorSearch%22%2C%22NN3FJMUV%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22LSTPFY1C%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22HHCPM1F8%22%3A%22gradeSearch%2CmajorSearch%22%2C%22OORPU5FE%22%3A%22gradeSearch%2CmajorSearch%22%2C%22WD0FTY92%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22BPBAFJ5S%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22EMRPGLPA%22%3A%22gradeSearch%2CmajorSearch%22%2C%22PWFIRAGL%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%22U8J8LYLV%22%3A%22gradeSearch%2CmajorSearch%22%2C%22R79MZMCB%22%3A%22gradeSearch%22%2C%22J708GVCE%22%3A%22gradeSearch%2CmajorSearch%22%2C%22HR1YT1Z9%22%3A%22gradeSearch%2CmajorSearch%22%2C%22JUP3MUPD%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22NLBO1Z6R%22%3A%22gradeSearch%2CmajorSearch%22%2C%22RMJLXHZ3%22%3A%22gradeSearch%2CmajorSearch%2CsentenceSearch%22%2C%221UR4K4HZ%22%3A%22gradeSearch%2CmajorSearch%2CauthorSearch%2CsentenceSearch%22%2C%22NB3BWEHK%22%3A%22gradeSearch%2CmajorSearch%22%2C%22XVLO76FD%22%3A%22gradeSearch%2CmajorSearch%22%7D; SID_restapi=018107; personsets=%E7%94%A8%E6%88%B7%E5%81%8F%E5%A5%BD%E8%AE%BE%E7%BD%AE%E4%B8%BA%E7%A9%BA%EF%BC%81; createtime-advInput=2025-04-18%2011%3A56%3A42; dblang=both; c_m_LinID=LinID=WEEvREcwSlJHSldTTEYyUFcra3BFQkV4cnpGQkIzOWNUbnlGYU9pMTd2VT0=$9A4hF_YAuvQ5obgVAqNKPCYcEjKensW4IQMovwHtwkF4VYPoHbKxJw!!&ot=05%2F18%2F2025%2011%3A56%3A21; c_m_expire=2025-05-18%2011%3A56%3A21; tfstk=gy3jMvwhQtXjiS6oiKdzOq",
-        "Host": "kns.cnki.net",
-        "Upgrade-Insecure-Requests": "1",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+        "connection": "keep-alive",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "origin": "https://kns.cnki.net",
+        "referer": "https://kns.cnki.net/kns8s/AdvSearch?crossids=YSTT4HG0,LSTPFY1C,JUP3MUPD,MPMFIG1A,WQ0UVIAA,BLZOG7CK,PWFIRAGL,EMRPGLPA,NLBO1Z6R,NN3FJMUV",
+        "sec-ch-ua": '"Microsoft Edge";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36 Edg/135.0.0.0",
+        "x-requested-with": "XMLHttpRequest"
     }
 
     def start_requests(self):
         for school in self.school.values:
             school = school[0]
-            for magazine in self.beida.values:
+            for magazine in self.nanda.values:
                 magazine = magazine[3]
                 zongfenlei=magazine[0][2:]       #去掉前两个编号 只取分类名称
                 school = school
@@ -88,37 +65,10 @@ class ZhiwangspiderSpDDider(scrapy.Spider):
                 #自动替换关键词
                 decoded_form_data = urllib.parse.parse_qs(data)         #把URL格式的表单字符串data转换成Scrapy能用的字典形式
                 data = {key: value[0] if value else "" for key, value in decoded_form_data.items()}
-                yield FormRequest(url='https://kns.cnki.net/kns8s/',cookies=self.cookies,headers=self.headers,callback=self.parse,formdata=data,cb_kwargs={'学校':school,'期刊':magazine,'总分类':zongfenlei})
+                yield FormRequest(url='https://kns.cnki.net/kns8s/brief/grid',cookies=self.cookies,headers=self.headers,callback=self.parse,formdata=data,cb_kwargs={'学校':school,'期刊':magazine,'总分类':zongfenlei})
                 #发送post请求，并交给self.parse() 来处理返回的搜索结果
 
     #对每一个【学校 + 期刊】组合，构造一个 POST 表单，模拟从知网“高级检索”搜索论文数量，并发出请求，交给 parse() 去处理返回结果。
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     def parse(self, response,**kwargs):
         response=response.text
@@ -136,13 +86,8 @@ class ZhiwangspiderSpDDider(scrapy.Spider):
         ite['期刊']=magazine
         ite['数量']=soup
         ite['总分类']=zongfenlei
-        ite['属于']='北核'
+        ite['属于']='南核'
         yield ite
 
-# todo
-"""
-添加了子进程
-这样就能直接在当前文件里运行
-"""
 if __name__ == '__main__':
-    subprocess.run(['scrapy', 'crawl', 'zhiwangspider'])
+    subprocess.run(['scrapy', 'crawl', 'zhiwangspider', '-o', 'data1.csv'])
